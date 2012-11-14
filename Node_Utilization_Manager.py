130f0733ee1dc7bbd49ef696b28b4e71e8e4fc31
Joshua Ferguson <jwfergus@asu.edu>
2012-11-14 11:45:46 -0700

    Basic comments and structure of Node-side management daemon

---
+++ Node_Utilization_Manager.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+#
+#	Node_Utilization_Manager.py
+#		11/14/12
+#	Daemon that uses a utilization schedule to control the CPU utilization
+#		of servers inside of BlueCenter
+#
+#	author: Joshua Ferguson
+#		jwfergus@asu.edu | joshuawferguson@gmail.com	
+
+
+import csv	# Read CSV files
+import os	# Lets us issue bash commands
+
+
+def read_schedule():
+
+
+def find_time_window_values():
+
+
+
+def main():
+
+
+
+
+if __name__ == "__main__":
+    main()
