1b17059bd8810d3cf99163a6e25f0961b9bac4bb
Joshua Ferguson <jwfergus@asu.edu>
2012-11-14 15:19:06 -0700

    major rehaul to Node Utilization Manager

--- Node_Utilization_Manager.py
+++ Node_Utilization_Manager.py
@@ -11,19 +11,76 @@
 
 import csv	# Read CSV files
 import os	# Lets us issue bash commands
+import sys	# Get command line arguments
+from datetime import datetime	# Naturally, lets us get the current time
+import time
 
 
-def read_schedule():
+def start_processes(number_of_processes, command_args):
+	process_list = list()
+	for i in range(number_of_processes):
+		process_list.append(subprocess.Popen(command_args))
+	return process_list
 
+def start_cpu_limit(util_procs, limit):
+	cpu_limit_procs = list()
+	for process in util_procs:
+		cpu_limit_args = ['/usr/bin/cpulimit',  '-p',  str(process.pid),  '-l',  str(limit)]
+		cpu_limit_procs.append(subprocess.Popen(cpu_limit_args))
+	return cpu_limit_procs
 
-def find_time_window_values():
-
-
+def kill_processes(process_list):
+	for process in process_list:
+		os.system("kill -9 " + str(process.pid))
 
 def main():
 
+	for process in cpu_limit_procs:
+		print "cpulimit process: " + str(process.pid) + " started."
+	time.sleep(6)
+	kill_processes(cpu_limit_procs)
+	print "cpulimit processes killed"
+	time.sleep(6)
 
+
+def test():
+
+		# Need to switch from 'with' to try/except at some point
+	with open(sys.argv[1], 'rb') as schedule_file:
+		schedule_reader = csv.reader(schedule_file)
+		schedule_reader.next() # Move past the first line of file (just a comment)
+		
+		#
+		# Start our utilization processes and store them in util_procs
+		#		
+		cwd = os.getcwd()
+		util_args = [(cwd+'/util')]
+		util_procs = start_processes(4, util_args)
+
+		#
+		# Start the initial cpu_limit processes at 100 limit
+		#
+		current_cpu_limit_procs = start_cpu_limit(util_procs, 100) 
+		while True:
+			current_time = int(datetime.now().strftime('%s'))
+			current_row = schedule_reader.next()
+			sleep_time_diff = (int(current_row[1]) - current_time)
+
+			#
+			#	Set Utilization
+			#
+			new_cpu_limit_procs = start_cpu_limit(util_procs,int(current_row[2]))
+			kill_processes(current_cpu_limit_procs)
+			current_cpu_limit_procs = new_cpu_limit_procs 
+
+			#
+			#	Sleep until next time window
+			#
+			print "Current Time Window: " + str(current_row) + "; sleep time: " + sleep_time_diff
+			time.sleep(sleep_time_diff)
+			
 
 
 if __name__ == "__main__":
-    main()
+#	main()
+	test()
