91f4617f2d9e753fe002ecc7be933304d7690b3b
Joshua Ferguson <jwfergus@asu.edu>
2012-11-14 19:30:51 -0700

    realized the past few bugs were due to expect now being installed on bootserver

--- populate_known_hosts.py
+++ populate_known_hosts.py
@@ -27,7 +27,7 @@ def generate_server_chassis_map():
 		if((" " + server[0].split(".")[3] + " ") in " 1 2 3 4 5 6 13 14 15 16 17 18 25 26 27 28 29 30 "):
 			continue
 		
-		os.system("expect " +str(os.getcwd())+"/populate_known_hosts.sh "+ server[0])
+		os.system("./populate_known_hosts.sh "+ server[0])
 
 
 if __name__ == "__main__":
