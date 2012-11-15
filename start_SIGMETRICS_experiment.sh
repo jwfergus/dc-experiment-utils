19b390221305e9f334bd2b00555e795977f79004
Joshua Ferguson <jwfergus@asu.edu>
2012-11-14 19:02:56 -0700

    small changes

--- start_SIGMETRICS_experiment.sh
+++ start_SIGMETRICS_experiment.sh
@@ -15,27 +15,26 @@ set pass "bluetool"
 
 # Get IP address as input
 set input_ip [lindex $argv 0]
-set filename [lindex $argv 1]
 set timeout 5
 # spawn the scp thread
-spawn scp $filename bluetool@$input_ip:/home/bluetool/
+spawn ssh bluetool@$input_ip
 
 
 # When prompted for a password, give it (MUST BE SET AT THE TOP OF THE FILE)
-expect "?" 
-send "yes\r"
 expect "password:" 
 send "$pass\r" 
 expect "$ "
-send "cd mutual-exclusion-logic\r" 
+send "mv Chassis* chassis.schedule\r"
+expect "$ "
+send "gcc -lpthread -o util utilization.c\r"
 expect "$ "
 send "sudo -k\r"
 expect "$ "
-send "make\r"
-expect "$ "
 send "sudo su\r"
-expect "mel:"
+expect "bluetool:"
 send "$pass\r" 
+expect "# "
+send "chmod +x util\r"
 expect "# "
 send "nohup python node_main.py &\r"
 expect "'"
